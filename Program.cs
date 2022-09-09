4b2ccf04a3eb5da56f8b507c5a1e874a003d781c
katy.korotkih <katy_korotkih@mail.ru>
2022-09-09 16:56:45 +0300

    Find elements added

--- Program.cs
+++ Program.cs
@@ -20,6 +20,20 @@ void ShowArray(string[] array)
     Console.WriteLine();
 }
 
+string[] StringLenght(string[] array, int max)
+{
+    string[] newArray = new string[array.Length];
+    bool empty = true;
+    for(int i = 0; i < newArray.Length; i++)
+        if (array[i].Length <= max ) 
+        {
+            newArray[i] = array[i];
+            empty = false;
+        }  
+    if (empty == true) Console.WriteLine("Таких элементов нет");
+    return newArray;                        
+}
+
 Console.WriteLine("Пожалуйста, введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
