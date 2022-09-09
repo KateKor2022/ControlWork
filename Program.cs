42b915623460af9973c862e9f46bc1502c2f57a0
katy.korotkih <katy_korotkih@mail.ru>
2022-09-09 16:54:26 +0300

    Input array added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] InputArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент из {size}: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.WriteLine("Пожалуйста, введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите максимальное количество символов элемента для отбора: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+string[] array = InputArray(size);
+Console.WriteLine();
